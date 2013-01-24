fea8d6225ee4cedec10ec636c0bbbc41a55b93c3
Hamza Faran <hamzafaran@outlook.com>
2013-01-24 01:53:24 -0800

    r14

--- Digirama Source/main.cpp
+++ Digirama Source/main.cpp
@@ -30,8 +30,8 @@ struct BackgroundLoader
    // static functions
    static Bool BackgroundLoad(Thread &thread) // this function will be called in the secondary thread to load the initial world data
    {
-      Time.wait(LOAD_TIME); // wait 2 seconds just for the needs of tutorial to make the loading screen not disappear too quickly
-      return false; // don't continue the thread
+	  Time.wait(LOAD_TIME); // wait 2 seconds just for the needs of tutorial to make the loading screen not disappear too quickly
+	  return false; // don't continue the thread
    }
 
    // members
@@ -40,33 +40,33 @@ struct BackgroundLoader
    // methods
    void del() // delete background loader
    {
-      thread.del();
+	  thread.del();
    }
    void start() // start background loader
    {
-      thread.create(BackgroundLoad); // start loader thread
+	  thread.create(BackgroundLoad); // start loader thread
    }
    Bool update() // update background loader
    {
-      if(!thread.created())return false; // not loading anything
-      customUpdate(); // call custom updating method
-      return true; // loading
+	  if(!thread.created())return false; // not loading anything
+	  customUpdate(); // call custom updating method
+	  return true; // loading
    }
    Bool draw() // draw background loader
    {
-      if(!thread.created())return false; // not loading anything
-      customDraw(); // call custom drawing method
-      if(!thread.active()) // thread is created but finished processing
-      {
-         thread.del(); // delete the thread
-         Renderer.setFade(1.0f); // enable screen fading from loading screen to the game
-      }
-      return true; // loading or finishing
+	  if(!thread.created())return false; // not loading anything
+	  customDraw(); // call custom drawing method
+	  if(!thread.active()) // thread is created but finished processing
+	  {
+		 thread.del(); // delete the thread
+		 Renderer.setFade(1.0f); // enable screen fading from loading screen to the game
+	  }
+	  return true; // loading or finishing
    }
 
    void customUpdate() // you can modify this method and perform custom updating of the loading screen
    {
-      Time.wait(1000/24); // limit main thread speed to 24 fps, to give more cpu power for background thread
+	  Time.wait(1000/24); // limit main thread speed to 24 fps, to give more cpu power for background thread
    }
    void customDraw() // you can modify this method and perform custom drawing of the loading screen
    {
