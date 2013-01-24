91a5a0ee0b9a576673012372332cecd97df82e24
Hamza Faran <hamzafaran@outlook.com>
2013-01-24 01:49:55 -0800

    r13

---
+++ Digirama Source/Player.cpp
@@ -0,0 +1,100 @@
+#include "stdafx.h"
+#include "Player.h"
+
+void Player::drawPlayer( )
+{
+	if(this->facingLeft) {
+		this->currentFrame->draw(Rect(this->x+2*this->pixelX/RES_Y, this->y, this->x, this->y+2*this->pixelY/RES_Y));
+	} else {
+		this->currentFrame->draw(Rect(this->x, this->y, this->x+2*this->pixelX/RES_Y, this->y+2*this->pixelY/RES_Y));
+	}
+}
+
+//1 = move left, 2 = move right, 3 = stop, 4 = jump
+void Player::movePlayer( int moveType )
+{
+	switch (moveType) {
+	case 1:
+		this->facingLeft = true;
+		this->x -= Time.d()*this->movementSpeed;
+
+		if(this->x < (Flt) -RES_X/RES_Y)
+			this->x = (Flt) -RES_X/RES_Y;
+
+		if(this->frameCounter<9)
+			this->frameCounter += 0.3333;
+		else
+			this->frameCounter=0;
+		break;
+	case 2:
+		this->facingLeft = false;
+		this->x += Time.d()*this->movementSpeed;
+
+		if(this->x+2*(Flt) this->pixelX/RES_Y > RES_X/RES_Y)
+			this->x = RES_X/RES_Y-2*this->pixelX/RES_Y;
+
+		if(this->frameCounter<9)
+			this->frameCounter += 0.3333;
+		else
+			this->frameCounter=0;
+		break;
+	case 3:
+		if(this->frameCounter<9)
+			this->frameCounter++;
+		break;
+	case 4:
+		this->jumping = true;
+		break;
+	}
+}
+
+void Player::handleJump( )
+{
+	if(this->jumping && this->jumpCount < 25) {
+		this->y += .0225f;
+		this->jumpCount++;
+	} else if(!this->jumping && this->jumpCount > 0) {
+		this->y -= .0225f; //Jump Speed
+		this->jumpCount--;
+	} else if(this->jumping && this->jumpCount >= 25) {
+		this->jumping = false;
+	}
+}
+
+void Player::handleInput( KB_BUTTON jump, KB_BUTTON left, KB_BUTTON right )
+{
+	if(Kb.b(jump) && this->jumpCount == 0) {
+		movePlayer(4);
+	}
+	if(Kb.b(left)) {
+		movePlayer(1);
+	}
+	else if(Kb.b(right)) {
+		movePlayer(2);
+	}
+	else {
+		movePlayer(3);
+	}
+}
+
+void Player::playerUpdate( KB_BUTTON jump, KB_BUTTON left, KB_BUTTON right )
+{
+	handleInput( jump, left, right );
+	handleJump( );
+	this->currentFrame = &this->frame[(int) this->frameCounter];
+}
+
+void Player::initPlayer( int pX, int pY, float moveSpeed )
+{
+	this->x = -RES_X/RES_Y * 0.9f;
+	this->y = -0.75f;
+	this->jumping = false;
+	this->jumpCount = 0;
+	this->facingLeft = false;
+	this->falling=true;
+	this->pixelX = pX;
+	this->pixelY = pY;
+	this->movementSpeed = moveSpeed;
+}
+
+
---
+++ Digirama Source/Player.h
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#define RES_Y 768.0f
+#define RES_X 1366.0f
+
+struct Player {
+	Flt x; //x-coordinate of Player
+	Flt y; //y-coordinate of Player
+	Image *currentFrame; //pointer to current frame of animation
+	bool jumping; 
+	int jumpCount;
+	bool facingLeft; //Indicates whether to flip when drawing
+	bool falling;
+	Flt frameCounter; //index of frame array for the current frame of animation
+	Image frame[10]; //Array of images holding the animations
+	int pixelX; //X-dimension of the frames in pixels
+	int pixelY; //Y-dimension of the frames in pixels
+	Flt movementSpeed; //movement speed of the Player (default = 1.0f)
+
+	void initPlayer( int pX, int pY, float moveSpeed=1.0f );
+	void handleInput( KB_BUTTON jump, KB_BUTTON left, KB_BUTTON right );
+	void playerUpdate( KB_BUTTON jump, KB_BUTTON left, KB_BUTTON right );
+	void movePlayer( int moveType ); //1 = move left, 2 = move right, 3 = stop, 4 = jump
+	void handleJump(  );
+	void drawPlayer(  );
+};
+
+
+
+
+#endif
+
