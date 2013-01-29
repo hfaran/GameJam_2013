43d35c81695fac2f8200b3ee91148281c60c3f73
Hamza Faran <hamzafaran@outlook.com>
2013-01-28 23:44:28 -0800

    r23a
    Implemented World struct in main; it works.

--- Digirama Source/main.cpp
+++ Digirama Source/main.cpp
@@ -1,23 +1,26 @@
 #include "stdafx.h"
-#include "Player.h"
 #include "main.h"
+#include "Player.h"
 #include "BackgroundLoader.h"
 #include "customPhysics.h"
 #include "mathFunctions.h"
+#include "World.hpp"
 #include <iostream>
 #include <string>
 #include <sstream>
 
 //Use this to enable or disable BGM
 #define BGM_ENABLED 0
+//Initialize Resolution to something
+float RES_X = 1366.0f;
+float RES_Y = 768.0f;
 
 //Invisible Assets
 MusicTheme mtIdle;
 // Objects
-Image bg; // background image object
-Player chip;
-Rect ground, platform[3];
 BackgroundLoader esenthelLogo;
+World world;
+
 
 //---------------------Engine-Functions--------------------------------//
 
@@ -30,18 +33,15 @@ void InitPre()
 
 Bool Init()
 {
-	//Load the background
-	bg.load("_Assets/ChipGame/gfx/lololol.gfx"); 
-	//Initialize Chip, the player
-	chip.initPlayer(93, 134);
-	//Initialize the platforms in the level
-	initPlatform( 1 );
-
+	{
 	//Load Esenthel Logo for startup
 	esenthelLogo.eLogo.load("_Assets/ChipGame/gfx/logo.gfx");
-
 	// Display the Esenthel logo
 	esenthelLogo.start(); 
+	}
+
+	//Initialize the world
+	world.InitWorld(93, 134, 1);
 
 	//Play BGM
 	if(BGM_ENABLED)
@@ -57,10 +57,12 @@ void Shut()
 
 Bool Update()
 {
-
+	//If Escape is pressed, quit
 	if(Kb.bp(KB_ESC))return false;
 
-	chip.playerUpdate( KB_UP, KB_LEFT, KB_RIGHT );
+	//Update the world
+	world.UpdateWorld();
+	//Keep the BGM going
 	Music.play(mtIdle);
 
 	return true;
@@ -68,28 +70,12 @@ Bool Update()
 
 void Draw()
 {
-	if(esenthelLogo.draw())return; //Allows the E logo to show
+	//Allows the E logo to show
+	if(esenthelLogo.draw())return; 
 
-	D.clear(BLACK);
-
-	// draw background
-	bg.draw(Rect((Flt) -RES_X/RES_Y, -1.0f, (Flt) RES_X/RES_Y, 1.0f));
-
-	chip.drawPlayer( );
+	//Draw the world
+	world.DrawWorld();
 }
 
-//---------------Non-engine Functions-----------------------------//
 
-void initPlatform( int Level )
-{
-	switch (Level) {
-	case 1:
-		ground = Rect(convertX(0), convertY(664), convertX(1366), convertY(768));
-		platform[0] = Rect(convertX(469), convertY(497), convertX(296), convertY(543));
-		platform[1] = Rect(convertX(1366), convertY(357), convertX(1019), convertY(405));
-		platform[2] = Rect(convertX(1366), convertY(497), convertX(727), convertY(543));
-		break;
-	}
-
-}
 
--- Digirama Source/main.h
+++ Digirama Source/main.h
@@ -1,11 +1,9 @@
+#include "stdafx.h"
+
 #ifndef MAIN_H
 #define MAIN_H
 
-#define RES_Y 768.0f
-#define RES_X 1366.0f 
-
-// Function prototypes
-
-void initPlatform( int Level );
+extern float RES_Y;
+extern float RES_X;
 
 #endif
